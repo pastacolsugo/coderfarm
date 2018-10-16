ce14a25e75cd08bcf2cc255779de1fecacc83f94
pastacolsugo <ugobaroncini@gmail.com>
2018-10-16 15:47:04 +0200

    update

---
+++ Lezione 3/array1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int numeri[10];
+
+	for (int i=0; i<10; i++){
+		cin >> numeri[i];
+	}
+
+	for (int i=0; i<10; i++) {
+		cout << numeri[i] << endl; 
+	}
+}
+
+
+
+
+
+
---
+++ Lezione 3/easy1.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+
+using namespace std;
+
+int main () {
+	int N;
+	int massimo;
+	cin >> N;
+
+	int lista[N];
+
+	for (int i=0; i<N; i++) {
+		cin >> lista[i];	
+	}
+
+	if (lista[0] > lista[1]) {
+		massimo = lista[0];
+	} else {
+		massimo = lista[1];
+	}
+
+	for (int i=3; i<N; i++) {
+		if (massimo < lista[i]){
+			massimo = lista[i];
+		}
+	}
+
+	cout << "Il maggiore e': " << massimo << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

d9d4f1c62f2195b789cc91a57084ec3f4dee3d95
pastacolsugo <ugobaroncini@gmail.com>
2018-10-23 16:35:37 +0200

    6B-c

---
+++ Lezione 3/soluzioni/esercizio6B-C.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N=0;
+	cin >> N;
+
+	for (int j=0; j<N; j++) {
+		for (int i=0; i<N; i++) { // ripetiamo N volte
+			cout << "#";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+
+
+
+
+
+

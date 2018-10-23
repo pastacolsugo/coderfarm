d2482983ffe35a4a17fa7a07c67c4cbcbed3fede
pastacolsugo <ugobaroncini@gmail.com>
2018-10-23 15:55:45 +0200

    sol Es 3B

---
+++ Lezione 3/soluzioni/esercizio3B-for.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int somma = 0;
+
+	for (int i=0; i<=1000; i++) {
+		somma += i;
+	}
+
+	cout << somma << endl;
+}
+
+
---
+++ Lezione 3/soluzioni/esercizio3B-formula.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+
+	cin >> N;
+
+	// formula della sommatoria
+
+	cout << (N * (N+1))/2 << endl; 
+}
+
+

e6183aca6b4029ad8f7787a766fc195d482b2579
Ugo Baroncini <ugobaroncini@gmail.com>
2018-11-27 14:25:49 +0100

    Calcolatrice WIP

---
+++ Lezione 9/calcolatrice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	cout << "Quale operazione vuoi eseguire?" << endl;
+ 	cout << "1. Addizione" << endl;
+ 	cout << "2. Sottrazione" << endl;
+ 	cout << "3. Moltiplicazione" << endl;
+ 	cout << "4. Divisione" << endl;
+ 	cout << "0. Esci" << endl;
+ }

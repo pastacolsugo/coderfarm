30c7a8baec4cc27735ce4ebe0aba333e458d754a
Ugo Baroncini <ugobaroncini@gmail.com>
2018-11-27 15:22:29 +0100

    Calcolatrice WIP

--- Lezione 9/calcolatrice.cpp
+++ Lezione 9/calcolatrice.cpp
@@ -17,48 +17,69 @@ float divisione (float a, float b) {
 	return a/b;
 }
 
+float quadrato (float a) {
+	// return a*a;
+	return moltiplicazione(a, a);
+}
+
 int main () {
 	int operazioneScelta = -1;
 	float a, b;
 
-	cout << "Quale operazione vuoi eseguire?" << endl;
- 	cout << "1. Addizione" << endl;
- 	cout << "2. Sottrazione" << endl;
- 	cout << "3. Moltiplicazione" << endl;
- 	cout << "4. Divisione" << endl;
- 	cout << "0. Esci" << endl;
+	while (true) {
+		cout << "Quale operazione vuoi eseguire?" << endl;
+	 	cout << "1. Addizione" << endl;
+	 	cout << "2. Sottrazione" << endl;
+	 	cout << "3. Moltiplicazione" << endl;
+	 	cout << "4. Divisione" << endl;
+	 	cout << "5. Quadrato" << endl;
+	 	cout << "0. Esci" << endl;
 
- 	cin >> operazioneScelta;
+	 	cin >> operazioneScelta;
 
- 	cout << "Inserisci i due operandi:";
- 	cin >> a >> b;
+	 	if (operazioneScelta == 0) {
+		 	cout << "ciao ciao" << endl;
+ 			return 0;
+ 		}
 
- 	switch (operazioneScelta) {
- 		case 1: 
- 			// addizione
- 			cout << "Somma: " << somma(a, b) << endl;
- 			break;
- 		case 2:
- 			// sottrazione
- 			cout << "Differenza: " << sottrazione(a, b) << endl;
- 			break;
- 		case 3:
- 			// moltiplicazione
- 			cout << "Prodotto: " << moltiplicazione(a, b) << endl;
- 			break;
- 		case 4:
- 			// divisione
- 			cout << "Divisione: " << divisione(a, b) << endl;
- 			break;
- 		case 0:
- 			// esci
- 			cout << "ciao ciao" << endl;
- 			break;
- 		default:
+ 		if (operazioneScelta < 1 || operazioneScelta > 5) {
  			// operazione non valida
  			cout << "operazione non valida" << endl;
- 			break;
- 	}
+ 			continue;
+ 		}
+
+	 	switch (operazioneScelta) {
+	 		case 1: 
+	 			// addizione
+			 	cout << "Inserisci i due operandi:";
+			 	cin >> a >> b;
+	 			cout << "Somma: " << somma(a, b) << endl;
+	 			break;
+	 		case 2:
+	 			// sottrazione
+			 	cout << "Inserisci i due operandi:";
+			 	cin >> a >> b;
+	 			cout << "Differenza: " << sottrazione(a, b) << endl;
+	 			break;
+	 		case 3:
+	 			// moltiplicazione	 			
+			 	cout << "Inserisci i due operandi:";
+			 	cin >> a >> b;
+	 			cout << "Prodotto: " << moltiplicazione(a, b) << endl;
+	 			break;
+	 		case 4:
+	 			// divisione
+			 	cout << "Inserisci i due operandi:";
+			 	cin >> a >> b;
+	 			cout << "Divisione: " << divisione(a, b) << endl;
+	 			break;
+	 		case 5:
+	 			// quadrato
+	 			cout << "Inserisci la base: ";
+	 			cin >> a;
+	 			cout << "Quadrato: " << quadrato (a) << endl;
+	 	}
+	}
 }
 
 

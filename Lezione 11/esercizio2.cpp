dd32115a9160108ef66a956e92e3b1b35fbd2b7f
Ugo Baroncini <ugobaroncini@gmail.com>
2018-12-11 15:30:34 +0100

    added solution 2

---
+++ Lezione 11/esercizio2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    vector<vector<int> > grafo;
+
+    int n, c;
+
+    cin >> n >> c;
+
+    for (int i=0; i<c; i++) {
+        int p, a;
+        cin >> p >> a;
+
+        grafo[p].push_back(a);
+        grafo[a].push_back(p);
+    }
+
+    int nodo;
+
+    cout << "Di che nodo vuoi visualizzare i collegamenti? ";
+    cin >> nodo;
+
+    for (int i=0; i<grafo[nodo].size(); i++) {
+        cout << grafo[nodo][i] << endl;
+    }
+
+    return 0;
+}

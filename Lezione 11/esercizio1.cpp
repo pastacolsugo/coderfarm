ce3f0f6f7c60bfbfe89ea87848c171ff57ab96a3
Ugo Baroncini <ugobaroncini@gmail.com>
2018-12-11 15:08:47 +0100

    soluzione esercizio 1

---
+++ Lezione 11/esercizio1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    vector<vector<int> > grafo;
+
+    int n, c;
+
+    cin >> n >> c;
+
+    for (int i=0; i<c; i++) {
+        int p, a;
+        cin >> p >> a;
+
+        grafo[p].push_back(a);
+        grafo[a].push_back(p);
+    }
+}
